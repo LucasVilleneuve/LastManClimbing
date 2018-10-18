85cf5a329f130be451aea7878a9ec0fa25ae7a5a
Wéry Paul <paul.wery@epitech.eu>
2018-10-19 08:22:25 +0900

    object BonusSpeed test

---
+++ ProtoLMC/Assets/Scripts/UsableObjects/BonusSpeed.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusSpeed : MonoBehaviour {
+
+    [SerializeField] private readonly float speedBonusValue = 10f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject entity = collision.gameObject;
+
+        if (entity.tag == "Player")
+        {
+            PlayerMovement playerMovement = entity.GetComponent<PlayerMovement>();
+
+            playerMovement.climbingSpeed += this.speedBonusValue;
+            playerMovement.maxJetpackVerticalVelocity += this.speedBonusValue;
+            Destroy(gameObject);
+        }
+    }
+}
