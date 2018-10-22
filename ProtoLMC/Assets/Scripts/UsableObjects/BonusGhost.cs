86fca488ec9fba978da08449f1fc6c27fadeac1f
Wéry Paul <paul.wery@epitech.eu>
2018-10-22 13:55:47 +0900

    New Bonuses
    - MalusSpeed
    - BonusGhost

---
+++ ProtoLMC/Assets/Scripts/UsableObjects/BonusGhost.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusGhost : CollectableObject {
+
+    [SerializeField] private int bonusDuration = 3;
+    
+    void Start () {
+        this.realtimeBeforeDestruction = this.bonusDuration;
+        this.actionOnTriggerEnter = DisablePlayerCollider;
+        this.actionBeforeDestruction = EnablePlayerCollider;
+    }
+	
+	void Update () {
+		
+	}
+
+    void DisablePlayerCollider(GameObject entity)
+    {
+        Color ghost = entity.GetComponent<SpriteRenderer>().color;
+
+        ghost.a = 0.2f;
+        entity.GetComponent<Collider2D>().enabled = false;
+        entity.GetComponent<SpriteRenderer>().color = ghost;
+    }
+
+    void EnablePlayerCollider(GameObject entity)
+    {
+        Color regular = entity.GetComponent<SpriteRenderer>().color;
+
+        regular.a = 1.0f;
+        entity.GetComponent<Collider2D>().enabled = true;
+        entity.GetComponent<SpriteRenderer>().color = regular;
+    }
+}
