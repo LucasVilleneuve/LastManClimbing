f69d65a912c15d0076bc2c34c72d28854aabd61d
Philippe bouttereux <philippe.bouttereux@epitech.eu>
2018-11-06 20:10:17 +0900

    Changed chunk size to 24/10 and starter to 6/10
    Also changed hardcoded values in script to const (CHUNK_W, CHUNK_H, STARTER_H)

--- Assets/Scripts/TilemapsManager.cs
+++ Assets/Scripts/TilemapsManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class TilemapsManager : MonoBehaviour {
 
+    public const int CHUNK_W = 10;
+    public const int CHUNK_H = 24;
+    public const int STARTER_H = 6;
+
+
     public GameObject starter;
     public GameObject[] maps;
     public float posX;
@@ -18,18 +23,20 @@ public class TilemapsManager : MonoBehaviour {
 
 	void Start () {
 
+        // Getting the preset scale for maps (and exiting if they have different ones)
         scale = maps[0].transform.localScale;
         foreach (GameObject map in maps)
         {
             if (map.transform.localScale != scale)
             {
-                //Debug.LogException(new System.Exception("tile chunks have different scales. Exiting..."));
                 throw new System.Exception("tile chunks have different scales. Exiting...");
             }
         }
 
-        totalSize.x = 8 * scale.x;
-        totalSize.y = 10 * scale.y;
+        // Setting relative size, pos and minimum posY of the chunks
+        // (depending on the scale, the parent's position and the posX parameter)
+        totalSize.x = CHUNK_W * scale.x;
+        totalSize.y = CHUNK_H * scale.y;
         posX *= totalSize.x;
 
         pos = new Vector3(
@@ -39,10 +46,11 @@ public class TilemapsManager : MonoBehaviour {
         );
 
         minY = pos.y - totalSize.y;
-        print(minY);
 
-        addStarter(new Vector3(0, -(totalSize.y / 2), 0) + pos);
-        addChunk(new Vector3(0, totalSize.y / 2, 0) + pos);
+
+        // Adding the starter chunk (empty, half the normal size) and the first chunk
+        addStarter(new Vector3(0, -(totalSize.y / 3)*2, 0) + pos);
+        addChunk(new Vector3(0, totalSize.y / 3, 0) + pos);
     }
 
     private void addStarter(Vector3 pos)
