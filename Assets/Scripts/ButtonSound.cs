1ab10c56b088e052e6846a11cdebd10603f37429
escornt <thomas.escorne@epitech.eu>
2018-11-06 15:43:37 +0900

    Main menu + Pause Menu + Player animation
    Main menu + Pause Menu + Player animation

---
+++ Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ButtonSound : MonoBehaviour, ISelectHandler, IPointerEnterHandler
+{
+
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        audioSource.Play();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("p"))
+        {
+            if (Time.timeScale != 0.00001f)
+            {
+                Time.timeScale = 0.00001f;
+                SceneManager.LoadScene("PauseHUD", LoadSceneMode.Additive);
+            }
+            else
+            {
+                Time.timeScale = 1f;
+                SceneManager.UnloadSceneAsync("PauseHUD");
+            }
+        }
+    }
+}
