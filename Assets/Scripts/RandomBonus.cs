1cc42e085ecb28446aa4ba903cd8c309f3a14c12
Lucas Villeneuve <lucas.villeneuve@epitech.eu>
2018-11-08 16:20:15 +0900

    Adding random bonuses

---
+++ Assets/Scripts/RandomBonus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBonus : MonoBehaviour
+{
+    /* Serialized fields */
+    [SerializeField] private GameObject[] bonuses;
+
+    private void OnTriggerEnter2D(Collider2D entity)
+    {
+        Debug.Log("Collision");
+        if (bonuses.Length <= 0)
+            Debug.Log("Error, there is no bonuses.");
+        else
+        {
+            GameObject go = Instantiate(bonuses[Random.Range(0, bonuses.Length)], transform.position, Quaternion.identity);
+            go.GetComponent<CollectableObject>().UseEffect(entity.gameObject);
+        }
+        Destroy(gameObject);
+    }
+}
