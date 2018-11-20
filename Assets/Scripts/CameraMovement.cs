e378374fb6df0f6f0b03f5e71ab4cb884e6680b7
Lucas Villeneuve <lucas.villeneuve@epitech.eu>
2018-11-20 18:26:08 +0900

    Adding speed waves

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,37 +1,94 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] private float cameraSpeed = 0.0f;
-    [SerializeField] private float speedIncreaseByTick = 0.0f;
-    [SerializeField] private float timeBeforeMoving = 5.0f;
+    [SerializeField] private float[] wavesSpeed = { 70.0f, 80.0f, 90.0f, 100.0f, 105.0f, 110.0f, 115.0f, 120.0f, 125.0f, 150.0f };
+    [SerializeField] private float waveLength = 20.0f;
+    [SerializeField] private float timeBeforeStarting = 3.0f;
+    [SerializeField] private GameObject textNextWave;
+    [SerializeField] private GameObject textCounterWave;
+
+    // Debug
+    [SerializeField] private float cameraSpeed;
 
     private bool cameraMovementEnable = true;
+    private int wave = 0;
+    private float timeLeft = 0;
+    private TextMeshProUGUI textMPCounterWave;
 
-    private void Awake()
+    private void Start()
     {
+        textNextWave.SetActive(false);
+        textMPCounterWave = textCounterWave.GetComponent<TextMeshProUGUI>();
+
+        timeLeft = waveLength;
+        ActivateNextWave();
     }
 
-    public void setCameraSpeed(float newSpeed) { cameraSpeed = newSpeed; }
+    public void SetCameraSpeed(float newSpeed)
+    {
+        cameraSpeed = newSpeed;
+    }
 
-    public float getCameraSpeed() { return cameraSpeed; }
+    public float GetCameraSpeed()
+    {
+        return cameraSpeed;
+    }
 
     private void Update()
     {
         if (cameraMovementEnable)
         {
-            if (timeBeforeMoving > 0.0f)
+            if (timeBeforeStarting > 0.0f)
             {
-                timeBeforeMoving -= Time.deltaTime;
+                timeBeforeStarting -= Time.deltaTime;
             }
             else
             {
+                UpdateTime();
+
+                if (timeLeft < 0.0f)
+                {
+                    ActivateNextWave();
+                    timeLeft = waveLength;
+                }
+
                 transform.position = transform.position + new Vector3(0, cameraSpeed / 1000.0f, 0);
-                cameraSpeed += (speedIncreaseByTick / 1000.0f);
             }
         }
+    }
+
+    private void UpdateTime()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft > 0.0f)
+        {
+            if (timeLeft < 3.0f)
+            {
+                textNextWave.SetActive(true);
+                textMPCounterWave.text = Mathf.Ceil(timeLeft).ToString();
+            }
+        }
+        else
+        {
+            // Deactivate
+            textNextWave.SetActive(false);
+            textMPCounterWave.text = "";
+        }
+        string textCounter = textMPCounterWave.text;
+    }
+
+    private void ActivateNextWave()
+    {
+        if (wavesSpeed.Length == 0 || wave > wavesSpeed.Length)
+        {
+            return; // No more waves to do
+        }
+
+        cameraSpeed = wavesSpeed[wave];
+        ++wave;
     }
 
     public void EnableMovement(bool enable)
