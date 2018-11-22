8ea5410daace8a8793911ff5b77537ea26185e61
Philippe bouttereux <philippe.bouttereux@epitech.eu>
2018-11-22 17:05:55 +0900

    Changed rocks behaviour
    There are now 2 rocks with random size and position

--- Assets/Scripts/UsableObjects/TrapFallingRocks.cs
+++ Assets/Scripts/UsableObjects/TrapFallingRocks.cs
@@ -20,15 +20,23 @@ public class TrapFallingRocks : CollectableObject {
         Vector3 cameraPosition = Camera.main.transform.position;
         float fallingRockHeight = fallingRockPrefab.GetComponent<SpriteRenderer>().bounds.size.y;
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        GameObject tmp;
 
         foreach (GameObject player in players)
         {
             if (player != entity)
             {
-                rockPosition = player.transform.position;
-                rockPosition.y = cameraPosition.y + Camera.main.orthographicSize + fallingRockHeight;
-                //GameObject newRock =
-                Instantiate(this.fallingRockPrefab, rockPosition, Quaternion.identity);
+                //rockPosition = player.transform.position;
+                //rockPosition.y = cameraPosition.y + Camera.main.orthographicSize + fallingRockHeight;
+                for (ushort i = 0; i<2; ++i)
+                {
+                    rockPosition = player.transform.position;
+                    rockPosition.y = cameraPosition.y + Camera.main.orthographicSize + fallingRockHeight;
+                    print("spawning rock");
+                    rockPosition.x += Random.Range(-10f, 10.0f);
+                    tmp = Instantiate(this.fallingRockPrefab, rockPosition, Quaternion.identity);
+                    tmp.transform.localScale = new Vector3(Random.Range(0.75f, 2.5f), Random.Range(0.75f, 2.5f), 0);
+                }
             }
         }
     }
