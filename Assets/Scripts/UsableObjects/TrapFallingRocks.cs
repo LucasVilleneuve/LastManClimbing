850de318eeaa4db2278c7861a75cfae14163c399
Wéry Paul <paul.wery@epitech.eu>
2018-11-27 17:04:43 +0900

    Fix issue TrapFallingRocks
    - Fix an issue with the scaling of Falling rocks -> the material came black after scaling

--- Assets/Scripts/UsableObjects/TrapFallingRocks.cs
+++ Assets/Scripts/UsableObjects/TrapFallingRocks.cs
@@ -6,6 +6,8 @@ public class TrapFallingRocks : CollectableObject {
 
     public GameObject fallingRockPrefab = null;
     [SerializeField] private int fallingRocksNumber = 2;
+    [SerializeField] private float fallingRocksMaxScale = 2.0f;
+    [SerializeField] private float fallingRocksMinScale = 1.0f;
 
     void Start () {
         this.actionOnTriggerEnter = InstantiateFallingRock;
@@ -30,15 +32,18 @@ public class TrapFallingRocks : CollectableObject {
             {
                 Bounds bounds = player.GetComponent<PlayerArea>().GetPlayerMapBounds();
 
-                for (ushort i = 0; i < fallingRocksNumber; i++)
+                Debug.Log(this.fallingRocksNumber);
+                for (ushort i = 0; i < this.fallingRocksNumber; i++)
                 {
-                    float newScale = Random.Range(0.5f, 1.0f);
-                    print("spawning rock");
+                    float newScale = Random.Range(this.fallingRocksMinScale, this.fallingRocksMaxScale);
+
                     rockPosition = new Vector3(bounds.min.x, topCameraPosition, 0);
-                    rockPosition.x += Random.Range(fallingRockBounds.size.x / 2, bounds.size.x - (fallingRockBounds.size.x / 2));
-                    rockPosition.y += Random.Range(0, fallingRockBounds.size.y * 2);
+                    rockPosition.x += Random.Range(
+                        fallingRockBounds.size.x * newScale / 2, 
+                        bounds.size.x - (fallingRockBounds.size.x * newScale / 2));
+                    rockPosition.y += Random.Range(0, fallingRockBounds.size.y * newScale * 2);
                     tmp = Instantiate(this.fallingRockPrefab, rockPosition, Quaternion.identity);
-                    //tmp.transform.localScale = new Vector3(newScale, newScale, 0); // TODO Scale create an issue with the material
+                    tmp.transform.localScale = tmp.transform.localScale * newScale;
                 }
             }
         }
