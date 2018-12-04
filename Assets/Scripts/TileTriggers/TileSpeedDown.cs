eee4b6f98bdf5f8ffb796845648ee4e90dda3b8e
Wéry Paul <paul.wery@epitech.eu>
2018-12-04 16:59:03 +0900

    Revert "Revert "Merge branch 'master' of https://github.com/LucasVilleneuve/LastManClimbing""
    This reverts commit 813a1ab3a263436c339debc1a5eab16366913ce8.

---
+++ Assets/Scripts/TileTriggers/TileSpeedDown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpeedDown : MonoBehaviour
+{
+
+    private float value = -5.0f;
+    private float duration = .5f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerMovement playerMovement = collision.gameObject.GetComponent<PlayerMovement>();
+
+            playerMovement.addSpeedModifier(new SpeedModifier(value, duration));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TileTriggers/TileSpeedUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpeedUp : MonoBehaviour {
+
+    private float value = 20.0f;
+    private float duration = 2.5f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerMovement playerMovement = collision.gameObject.GetComponent<PlayerMovement>();
+
+            playerMovement.addSpeedModifier(new SpeedModifier(value, duration));
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    	
+	}
+}
